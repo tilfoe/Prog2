36b1c070909dfda1eab9221bda5c1e0eb5bd9958
tilfoe <til.foerster@hotmail.com>
2020-04-26 20:10:10 +0200

    Code changes

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from noteapp.app import app  #imports the app
-
-if __name__ == "__main__": #if the name is equal the file it will do the following
-	app.run(debug=True)  #debug to get errors in the consol
--- noteapp/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask   #imports Flask
-from noteapp.views.index import bp as index_bp #imports the blueprint
-from noteapp.views.createnote import bp as createnote_bp
-
-app = Flask(__name__)    #titel, name of this file
-
-app.register_blueprint(index_bp) #registers the application
-app.register_blueprint(createnote_bp)
